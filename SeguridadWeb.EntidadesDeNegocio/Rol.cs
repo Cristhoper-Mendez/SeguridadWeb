61c1a71c568b45543b64288247d9f08d6097201e
Cristhoper Mendez <51303481+Cristhoper-Mendez@users.noreply.github.com>
2021-03-22 18:34:13 -0600

    Clase de Rol creada con sus datos y sus DataAnnotations

--- SeguridadWeb.EntidadesDeNegocio/Rol.cs
+++ SeguridadWeb.EntidadesDeNegocio/Rol.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SeguridadWeb.EntidadesDeNegocio
 {
     public class Rol
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+
+        public List<Usuario> Usuarios { get; set; }
     }
 }
