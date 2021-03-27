529fdc7879dc2f133acb428a9391f44850fc617e
Cristhoper Mendez <51303481+Cristhoper-Mendez@users.noreply.github.com>
2021-03-27 15:48:12 -0600

    Programar la capa de acceso a datos Programando la clase DBContexto para configurar Entity Framework

--- SeguridadWeb.AccesoADatos/BDContexto.cs
+++ SeguridadWeb.AccesoADatos/BDContexto.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 
+using Microsoft.EntityFrameworkCore;
+using SeguridadWeb.EntidadesDeNegocio;
+
 namespace SeguridadWeb.AccesoADatos
 {
-    public class BDContexto
+    public class BDContexto: DbContext
     {
+        public DbSet<Rol> Rol { get; set; }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-URUNQ5U\SQLEXPRESS;Initial Catalog=Facturacion;Integrated Security=True");
+        }
     }
 }
